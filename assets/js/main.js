304c7dbf977bed60281e5528ed3240e3e8d009d4
Md. Saiful <saifulpro247@gmail.com>
2023-08-09 04:09:10 +0600

    solve pie

--- assets/js/main.js
+++ assets/js/main.js
@@ -52,26 +52,208 @@
         chart.render();
 
         // chart two pie
-        var options = {
-          series: [88, 22],
-          chart: {
-            width: 200,
-            type: 'donut',
-          },
-        fill: {
-          type: 'gradient',
-        },
-        legend: {
-          show: false
-        },
-        
-        };
 
+        // var options = {
+        //   chart: {
+        //     width: 200,
+        //     type: 'donut',
+        //   },
+        //   series: [88, 22],
+        //   dataLabels: {
+        //     enabled: false,
+        //   },
+        //   stroke: {
+        //     show: true,
+        //     width: 9,
+        //     curve: 'smooth',
+        //     lineCap: 'round',
+        //   },
+        //   fill: {
+        //     type: ['solid', 'gradient'],
+        //   },
+        //   legend: {
+        //     show: false,
+        //   },
+        //   plotOptions: {
+        //     donut: {
+        //       size: 200
+        //     }
+        //   }
+        // };
+        
+        // var chart = new ApexCharts(document.querySelector("#sold-pie"), options);
+        // chart.render();
+        
+
+        // 
+
+        // var options = {
+        //   series: [75],
+        //   chart: {
+        //     height: 150,
+        //     type: "radialBar",
+        //     toolbar: {
+        //       show: false
+        //     }
+        //   },
+        //   plotOptions: {
+        //     radialBar: {
+        //       startAngle: 0,
+        //       endAngle: 360,
+        //       track: {
+        //         background: "#EDEFF1"
+        //       },
+        //       dataLabels: {
+        //         show: true,
+        //         name: {
+        //           show: false
+        //         },
+        //         value: {
+        //           formatter: function (val) {
+        //             return parseInt(val) + "%";
+        //           },
+        //           color: "#AE9142",
+        //           fontSize: "18px",
+        //           show: true,
+        //           fontWeight: "bold",
+        //           offsetY: 5
+        //         }
+        //       }
+        //     }
+        //   },
+        //   fill: {
+        //     colors: ["gradiant"]
+        //   },
+        //   stroke: {
+        //     show: true,
+        //     lineCap: "round",
+        //     width: 9,
+        //   }
+        // };
+        
+        // var chart = new ApexCharts(document.querySelector("#sold-pie"), options);
+        // chart.render();
+
+        // pie 1
+        var options = {
+          series: [75],
+          chart: {
+            height: 200,
+            width: 115,
+            type: "radialBar",
+            toolbar: {
+              show: false
+            }
+          },
+          plotOptions: {
+            radialBar: {
+              startAngle: 0,
+              endAngle: 360,
+              track: {
+                background: "#EDEFF1"
+              },
+              dataLabels: {
+                show: true,
+                name: {
+                  show: false
+                },
+                value: {
+                  formatter: function (val) {
+                    return parseInt(val) + "%";
+                  },
+                  color: "#AE9142",
+                  fontSize: "18px",
+                  show: true,
+                  fontWeight: "bold",
+                  offsetY: 5
+                }
+              }
+            }
+          },
+          fill: {
+            type: "gradient", 
+            gradient: {
+              shade: "dark", 
+              type: "horizontal", 
+              shadeIntensity: 0.8, 
+              gradientToColors: ["#07AC72"], 
+              inverseColors: true, 
+              opacityFrom: 0.5, 
+              opacityTo: 1, 
+              stops: [0, 100], 
+            }
+          },
+          stroke: {
+            show: true,
+            lineCap: "round",
+            width: 9,
+          }
+        };
+        
         var chart = new ApexCharts(document.querySelector("#sold-pie"), options);
         chart.render();
 
-
-
+        // pie 2
+        var options = {
+          series: [75],
+          chart: {
+            height: 200,
+            width: 115,
+            type: "radialBar",
+            toolbar: {
+              show: false
+            }
+          },
+          plotOptions: {
+            radialBar: {
+              startAngle: 0,
+              endAngle: 360,
+              track: {
+                background: "#EDEFF1"
+              },
+              dataLabels: {
+                show: true,
+                name: {
+                  show: false
+                },
+                value: {
+                  formatter: function (val) {
+                    return parseInt(val) + "%";
+                  },
+                  color: "#AE9142",
+                  fontSize: "18px",
+                  show: true,
+                  fontWeight: "bold",
+                  offsetY: 5
+                }
+              }
+            }
+          },
+          fill: {
+            type: "gradient", 
+            gradient: {
+              shade: "dark", 
+              type: "horizontal", 
+              shadeIntensity: 0.8, 
+              gradientToColors: ["#07AC72"], 
+              inverseColors: true, 
+              opacityFrom: 0.5, 
+              opacityTo: 1, 
+              stops: [0, 100], 
+            }
+          },
+          stroke: {
+            show: true,
+            lineCap: "round",
+            width: 9,
+          }
+        };
+        
+        var chart = new ApexCharts(document.querySelector("#pending-pie"), options);
+        chart.render();
+        
+        
+        
         
     })
 
