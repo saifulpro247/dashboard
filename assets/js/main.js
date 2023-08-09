bd201d18a1a5b2efd6860ef6edd1e03cf8b33a86
Md. Saiful <saifulpro247@gmail.com>
2023-08-09 13:54:54 +0600

    this

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,88 +51,6 @@
         var chart = new ApexCharts(document.querySelector("#month-chart"), options);
         chart.render();
 
-        // chart two pie
-
-        // var options = {
-        //   chart: {
-        //     width: 200,
-        //     type: 'donut',
-        //   },
-        //   series: [88, 22],
-        //   dataLabels: {
-        //     enabled: false,
-        //   },
-        //   stroke: {
-        //     show: true,
-        //     width: 9,
-        //     curve: 'smooth',
-        //     lineCap: 'round',
-        //   },
-        //   fill: {
-        //     type: ['solid', 'gradient'],
-        //   },
-        //   legend: {
-        //     show: false,
-        //   },
-        //   plotOptions: {
-        //     donut: {
-        //       size: 200
-        //     }
-        //   }
-        // };
-        
-        // var chart = new ApexCharts(document.querySelector("#sold-pie"), options);
-        // chart.render();
-        
-
-        // 
-
-        // var options = {
-        //   series: [75],
-        //   chart: {
-        //     height: 150,
-        //     type: "radialBar",
-        //     toolbar: {
-        //       show: false
-        //     }
-        //   },
-        //   plotOptions: {
-        //     radialBar: {
-        //       startAngle: 0,
-        //       endAngle: 360,
-        //       track: {
-        //         background: "#EDEFF1"
-        //       },
-        //       dataLabels: {
-        //         show: true,
-        //         name: {
-        //           show: false
-        //         },
-        //         value: {
-        //           formatter: function (val) {
-        //             return parseInt(val) + "%";
-        //           },
-        //           color: "#AE9142",
-        //           fontSize: "18px",
-        //           show: true,
-        //           fontWeight: "bold",
-        //           offsetY: 5
-        //         }
-        //       }
-        //     }
-        //   },
-        //   fill: {
-        //     colors: ["gradiant"]
-        //   },
-        //   stroke: {
-        //     show: true,
-        //     lineCap: "round",
-        //     width: 9,
-        //   }
-        // };
-        
-        // var chart = new ApexCharts(document.querySelector("#sold-pie"), options);
-        // chart.render();
 
         // pie 1
         var options = {
@@ -310,7 +228,83 @@
         var chart = new ApexCharts(document.querySelector("#tour-pie"), options);
         chart.render();
         
+        // static chart
+        var options = {
+          chart: {
+            height: 350,
+            type: "line",
+            stacked: false,
+            borderRadius: 10,
+            toolbar: {
+              show: false,
+            },
+          },
+          dataLabels: {
+            enabled: false
+          },
+          colors: ['#99C2A2', '#C5EDAC'],
+          series: [
+            
+            {
+              name: 'Sold',
+              type: 'column',
+              data: [21.1, 23, 33.1, 34, 44.1, 44.9, 56.5, 58.5]
+            },
+            {
+              name: 'Canceled',
+              type: 'column',
+              data: [10, 19, 27, 26, 34, 35, 40, 38]
+            },
+          ],
+          stroke: {
+            show: true,
+            width: [4, 4],
+            lineCap: 'round',
+          },
+          plotOptions: {
+            bar: {
+              columnWidth: "20%"
+              
+            }
+          },
+          xaxis: {
+            categories: [2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016]
+          },
+          
+          tooltip: {
+            shared: false,
+            intersect: true,
+            x: {
+              show: false
+            }
+          },
+          legend: {
+            position: "top",
+            horizontalAlign: "right",
+            offsetX: 40
+          }
+        };
         
+        
+        var chart = new ApexCharts(document.querySelector("#static-chart"), options);
+        
+        chart.render();
+        
+        // customer slider
+
+        const swiper = new Swiper('.customer--review-slider', {
+          // Optional parameters
+          direction: 'horizontal',
+          loop: true,
+        
+          // Navigation arrows
+          // navigation: {
+          //   nextEl: '.swiper-button--next',
+          //   prevEl: '.swiper-button--prev',
+          // },
+
+        
+        });
         
         
     })
